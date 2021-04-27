a3fdbfc64b15cf9dbc04bf29c2fbc5c4fb9e2440
qian <1906711512@qq.com>
2021-04-27 14:10:34 +0800

    修改了主启动类

--- src/main/java/com/qian/blog/BlogApplication.java
+++ src/main/java/com/qian/blog/BlogApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BlogApplication {
     public static void main(String[] args) {
+        System.out.println("hello world");
         SpringApplication.run(BlogApplication.class, args);
     }
 
