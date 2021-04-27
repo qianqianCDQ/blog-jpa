4f475bb50f6bbcd7791bacbb7bec78648052cd88
qian <1906711512@qq.com>
2021-04-27 14:08:15 +0800

    第一次提交

--- src/main/java/com/qian/blog/BlogApplication.java
+++ src/main/java/com/qian/blog/BlogApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BlogApplication {
-
     public static void main(String[] args) {
-        System.out.println("hello world");
         SpringApplication.run(BlogApplication.class, args);
     }
 
